af2903b4ca59a820b2019f21158738dfee659cff
Victor Borisov <victor.borisov@enavexpert.com>
2025-04-22 07:31:52 +0300

    initial back

---
+++ todo-back/main.py
@@ -0,0 +1,42 @@
+from contextlib import asynccontextmanager
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import select, func
+
+from models import init_db, Task
+import requests as rq
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    print('Bot is ready')
+    yield
+
+
+app = FastAPI(title="Todo app", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/tasks/{tg_id}")
+async def tasks(tg_id: int):
+    user = await rq.add_user(tg_id)
+    return await rq.get_tasks(user.id)
+
+@app.get('/api/main/{tg_id}')
+async def profile(tg_id: int):
+    user = await rq.add_user(tg_id)
+    count = get_compeleted_task_count(user.id)
+    return {'completedTasks': count}
+
+async def get_compeleted_task_count(user_id: int):
+    async with rq.async_session() as session:
+        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True, Task.user_id == user_id))
+
---
+++ todo-back/requests.py
@@ -0,0 +1,28 @@
+from sqlalchemy import select, update, delete, func
+from models import async_session, User, Task
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+class TaskScema(BaseModel):
+    id: int
+    title: str
+    completed: bool
+    user: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+async def add_user(tg_id: int):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        if not user:
+            user = User(tg_id=tg_id)
+            session.add(user)
+            await session.commit()
+            await session.refresh(user)
+        return user
+    
+async def get_tasks(user_id: int):
+    async with async_session() as session:
+        tasks = await session.scalars(select(Task).where(Task.user == user_id, Task.completed == False))
+        serialized_tasks = [TaskScema.model_validate(task).model_dump() for task in tasks]
+        return serialized_tasks
